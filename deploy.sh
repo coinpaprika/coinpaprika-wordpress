d27c97a1f91b8c3b2ade666ec0fe0d7889122e14
Dominique Stranz <dstranz@greywizard.com>
2018-09-17 14:28:46 +0200

    update deploy to wordpress scripts

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+if [ -z "$1" ]
+  then
+    echo "Usage: deploy.sh ../svn-checkout-directory."
+    exit 1
+fi
 
-zip -r coinpaprika.zip coinpaprika/ -x "*.git*" "*.DS_Store"
+OUTPUTDIR="$1"
+rsync -av --progress coinpaprika/* $OUTPUTDIR/trunk --exclude "*.git*" --exclude "*.DS_Store"
+rsync -av --progress assets $OUTPUTDIR --exclude "*.git*" --exclude "*.DS_Store"
